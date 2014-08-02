f417f33df23a497f4a41be2ec183cdd23d3fae21
prbodas <Prachi@MacBook-Air.local>
2014-08-02 16:10:20 -0700

    Blank mac app

---
+++ MyoCam/MyoCam/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyoCam
+//
+//  Created by Prachi Bodas on 8/2/14.
+//  Copyright (c) 2014 HITapp. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MyoCam/MyoCam/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MyoCam
+//
+//  Created by Prachi Bodas on 8/2/14.
+//  Copyright (c) 2014 HITapp. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
